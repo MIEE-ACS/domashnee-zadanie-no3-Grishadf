9034d5a483bf21e22713d2880d6f29cf8c68a931
Grishadf <74361972+Grishadf@users.noreply.github.com>
2022-10-11 20:05:35 +0300

    Add files via upload

---
+++ Domashka_3/Domashka_3/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿
+namespace Domashka_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(171, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(476, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " Приложение для проверки знаний таблицы умножения";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(113, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(220, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Вычислите произведение";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(211, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "*";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(113, 244);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(208, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Выберите верный ответ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(237, 163);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(61, 31);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "Y";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(144, 163);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(61, 31);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "X";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(86, 359);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(114, 31);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "Проверка...";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(510, 344);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(278, 61);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Проверить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(86, 283);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 29);
+            this.radioButton1.TabIndex = 13;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(159, 283);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 29);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.Text = "2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(230, 283);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(47, 29);
+            this.radioButton3.TabIndex = 15;
+            this.radioButton3.Text = "3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(306, 283);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(47, 29);
+            this.radioButton4.TabIndex = 16;
+            this.radioButton4.Text = "4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(712, 163);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(85, 31);
+            this.textBox4.TabIndex = 17;
+            this.textBox4.Text = "0";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(712, 123);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(85, 31);
+            this.textBox5.TabIndex = 18;
+            this.textBox5.Text = "0";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(521, 166);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(169, 25);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Неверных ответов:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(521, 126);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(147, 25);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Верных ответов:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(625, 82);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 25);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Счётчик";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Domashka_3/Domashka_3/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Domashka_3
+{
+    public partial class Form1 : Form
+    {
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void Form1_Load(object sender, EventArgs e)
+        {
+            Text = "Домашняя работа №3. УТС-21. Пыльнюк Николай";
+
+            label1.Text = "Приложение для проверки знаний таблицы умножения";
+            label2.Text = "Вычислите произведение";
+
+            button1.Text = "Начать";
+
+            textBox4.Text = "0";
+            textBox5.Text = "0";
+
+            radioButton1.Text = "A";
+            radioButton2.Text = "B";
+            radioButton3.Text = "C";
+            radioButton4.Text = "D";
+        }
+
+
+
+        void generation_example()       //твоя функция генерации
+        {
+            // —------— Рандомайзер, выдающий ответы —--------
+            Random rnd1 = new Random();         //Создание объекта для генерации чисел answer
+            int value1 = rnd1.Next(1, 10);      //Получить случайное число (в диапазоне от 1 до 9)
+
+            Random rnd2 = new Random();
+            int value2 = rnd2.Next(1, 10);
+
+            Random rnd3 = new Random();
+            int answer1 = rnd3.Next(1, 82);     //Получить случайное число (в диапазоне от 1 до 81)
+
+            Random rnd4 = new Random();
+            int answer2 = rnd4.Next(1, 82);
+
+            Random rnd5 = new Random();
+            int answer3 = rnd5.Next(1, 82);
+
+
+            // —------— Рандомайзер на позицию правильного ответа —--------
+            Random rnd6 = new Random();
+            int position = rnd6.Next(1, 5);
+        } 
+
+        bool is_second_click = false;           //переменная в глобальной области видимости
+
+
+
+        void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            if (button1.Text == "Начать")
+            {
+                button1.Text = "Проверить";
+            }
+
+
+            //проверка состояния флага is_second_click
+            if (is_second_click)
+            {
+                //вызов функции генерации, когда is_second_click == true
+                generation_example();
+            }
+            //смена состояния флага is_second_click
+            is_second_click = !is_second_click;
+
+
+            
+
+
+            textBox3.Text = Convert.ToString(value1);
+            textBox2.Text = Convert.ToString(value2);
+
+
+            // ---------- С помощью рандомайзера определяем позиции правильного и неправильных ответов ----------
+            if (position == 1)
+            {
+                radioButton1.Text = Convert.ToString(value1 * value2);
+
+                radioButton2.Text = Convert.ToString(answer1);
+                radioButton3.Text = Convert.ToString(answer2);
+                radioButton4.Text = Convert.ToString(answer3);
+            }
+
+            else if (position == 2)
+            {
+                radioButton2.Text = Convert.ToString(value1 * value2);
+
+                radioButton1.Text = Convert.ToString(answer1);
+                radioButton3.Text = Convert.ToString(answer2);
+                radioButton4.Text = Convert.ToString(answer3);
+            }
+
+            else if (position == 3)
+            {
+                radioButton3.Text = Convert.ToString(value1 * value2);
+
+                radioButton1.Text = Convert.ToString(answer1);
+                radioButton2.Text = Convert.ToString(answer2);
+                radioButton4.Text = Convert.ToString(answer3);
+            }
+
+            else if (position == 4)
+            {
+                radioButton4.Text = Convert.ToString(value1 * value2);
+
+                radioButton1.Text = Convert.ToString(answer1);
+                radioButton2.Text = Convert.ToString(answer2);
+                radioButton3.Text = Convert.ToString(answer3);
+            }
+            
+
+
+
+
+            // ---------- Проверяем правильность выбранного ответа ----------
+
+            if (radioButton1.Checked == true)
+            {
+                if (Convert.ToInt32(radioButton1.Text) == value1 * value2)
+                {
+                    textBox1.Text = "Верно!";
+                    textBox1.BackColor = Color.LimeGreen;
+                    textBox5.Text = Convert.ToString(Convert.ToInt32(textBox5.Text) + 1);
+                }
+                else
+                {
+                    textBox1.Text = "Не верно!";
+                    textBox1.BackColor = Color.OrangeRed;
+                    textBox4.Text = Convert.ToString(Convert.ToInt32(textBox4.Text) + 1);
+                }
+            }
+
+            if (radioButton2.Checked == true)
+            {
+                if (Convert.ToByte(radioButton2.Text) == value1 * value2)
+                {
+                    textBox1.Text = "Верно!";
+                    textBox1.BackColor = Color.LimeGreen;
+                    textBox5.Text = Convert.ToString(Convert.ToInt32(textBox5.Text) + 1);
+                }
+                else
+                {
+                    textBox1.Text = "Не верно!";
+                    textBox1.BackColor = Color.OrangeRed;
+                    textBox4.Text = Convert.ToString(Convert.ToInt32(textBox4.Text) + 1);
+                }
+            }
+
+            if (radioButton3.Checked == true)
+            {
+                if (Convert.ToByte(radioButton3.Text) == value1 * value2)
+                {
+                    textBox1.Text = "Верно!";
+                    textBox1.BackColor = Color.LimeGreen;
+                    textBox5.Text = Convert.ToString(Convert.ToInt32(textBox5.Text) + 1);
+                }
+                else
+                {
+                    textBox1.Text = "Не верно!";
+                    textBox1.BackColor = Color.OrangeRed;
+                    textBox4.Text = Convert.ToString(Convert.ToInt32(textBox4.Text) + 1);
+                }
+            }
+
+            if (radioButton4.Checked == true)
+            {
+                if (Convert.ToByte(radioButton4.Text) == value1 * value2)
+                {
+                    textBox1.Text = "Верно!";
+                    textBox1.BackColor = Color.LimeGreen;
+                    textBox5.Text = Convert.ToString(Convert.ToInt32(textBox5.Text) + 1);
+                }
+                else
+                {
+                    textBox1.Text = "Не верно!";
+                    textBox1.BackColor = Color.OrangeRed;
+                    textBox4.Text = Convert.ToString(Convert.ToInt32(textBox4.Text) + 1);
+                }
+            }
+
+           
+        
+
+        }
+
+
+
+
+    }
+}
+
